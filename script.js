754c9285ffb66e18e562e22bb0da739efa75c6a3
Pheonix-Nate <89256921+Pheonix-Nate@users.noreply.github.com>
2021-08-26 12:16:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let firstcar = document.getElementById('first_card');
+firstcar.addEventListener('click', ()=>{
+    alert('Incomplete')
+})
+let secondcar = document.getElementById('second_card');
+secondcar.addEventListener('click', ()=>{
+    alert('Incomplete')
+})
+let thirdcar = document.getElementById('third_card');
+thirdcar.addEventListener('click', ()=>{
+    alert('Incomplete')
+})
+let sold_out = document.getElementById('sold_out');
+sold_out.addEventListener('click', ()=>{
+    alert('This has been bought. Please wait for next restock')
+})
+let sales = document.getElementById('sales');
+sales.addEventListener('click', ()=>{
+    alert('Check out your graph. There is some info about sales, totals and more!')
+})
+let mail = document.getElementById('mail');
+mail.addEventListener('click', ()=>{
+    alert('YOU NEED RESTOCK, ELSE YOUR SITE WILL GO DOWNHILL!!')
+})
+let notify = document.getElementById('notify');
+notify.addEventListener('click', ()=>{
+    alert('No notification for you here.')
+})
